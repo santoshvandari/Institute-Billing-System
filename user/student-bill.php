4f768294e13233312763a2bc58b22ed4b5b035ad
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-29 15:08:30 +0545

    Initial

--- user/student-bill.php
+++ user/student-bill.php
@@ -3,6 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,6 +12,7 @@
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets//css/student-bill.css">
 </head>
+
 <body>
     <header>
         <nav>
@@ -22,40 +24,53 @@
                 </div>
             </div>
         </nav>
-       </header>
-       <main>
+    </header>
+    <main>
         <section class="side-option">
-           <div class="side-option-container">
-            <h4>User Dashboard</h4>
-            <ul class="side-option-list">
-                <li><a href="user-dashboard.php">Home</a></li>
-                <li><a href="student-bill.php">Bill</a></li>
-                <li><a href="student-list.php">Student</a></li>
-                <li><a href="add-student.php">Add Student</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-            </div> 
+            <div class="side-option-container">
+                <h4>User Dashboard</h4>
+                <ul class="side-option-list">
+                    <li><a href="user-dashboard.php">Home</a></li>
+                    <li><a href="student-bill.php">Bill</a></li>
+                    <li><a href="student-list.php">Student</a></li>
+                    <li><a href="add-student.php">Add Student</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
         </section>
         <section class="main-container">
             <div class="search-option">
-                <form action="#">
+                <form method="POST">
                     <label for="phone">Phone Number:</label>
-                    <input type="text" name="phone" id="phone" pattern="[0-9]{10}" required/>
-                    <button type="submit">Search</button>
-                </form> 
+                    <input type="text" name="phone" id="phone" pattern="[0-9]{10}" required />
+                    <button type="submit" name='search'>Search</button>
+                </form>
             </div>
-            <!-- <div class="bill-container">
-                <div class="table-container">
-                    <table border="1">
-                        <tr>
-                            <td colspan="6">
-                                Institute In fo
-                            </td>
-                        </tr>
-                    </table>
+            <div class="userinfo-container">
+                <div class="userinfo-wrapper">
+                    <div class="userinfo">
+                        <p>Name: Santosh Bhandari</p>
+                        <p>Address: Kanakai-07</p>
+                        <p>Number: 9824988945</p>
+                    </div>
+                    <div class="billinfo">
+                        <table border='1'>
+                            <tr>
+                                <th>S.N.</th>
+                                <th>Description</th>
+                                <th>Paid Amount</th>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Computer Basic</td>
+                                <td>5000</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
-            </div> -->
+            </div>
         </section>
-        </main>
+    </main>
 </body>
+
 </html>
