7da7676cf50b2c1bf78ac88c1a90b52c9a13c1aa
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-01 17:27:56 +0545

    Initial

--- admin/admin-login.php
+++ admin/admin-login.php
@@ -9,51 +9,56 @@
     <link rel="stylesheet" href="../assets/css/login.css" />
 </head>
 <body>
-session_start();
-                if(!empty($_SESSION['username'])){
-                    header("Location: user-dashboard.php");
-                }
+
+        <?php
                 include_once "../assets/database/connection.php";
-                    if ($_SERVER['REQUEST_METHOD']=='POST'){
-                            if(isset($_POST['submit'])){
-                                $username=trim($_POST['username']);
-                                $password=trim($_POST['password']);
-                                if (!$username){
-                                    echo "<p>* Please enter username</p>";
-                                }
-                                if (!$password){
-                                    echo "<p>* Please enter password</p>";
-                                }
-                                if($username && $password){
-                                    $read = 'select * from UserInfo WHERE username ="'.$username.'" and userpwd="'.$password.'";';
-                                    if($result=$con->query($read)){
-                                        if($result->num_rows>0){
-                                            $_SESSION["username"] = $username;
-                                            echo $_SESSION["username"];
-                                            header("Location: user-dashboard.php");
-                                        }else{
-                                            echo "<p>* Invalid username or password.</p>";
-                                        }
-                                    }else{
-                                        echo "error.";
-                                    }
-                                }
+                session_start();
+                        if(!empty($_SESSION['username'])){
+                            header("Location: admin-dashboard.html");
+                        }
+                          
 
-
-                            // if($username=='admin' && $password=='admin'){
-                            //     header('location:admin.html');
-                            // }
-                            // else{
-                            //     echo 'Invalid username or password';
-                            // }
-                            }
-                    }
-
-
+        ?>
     <div class="form-wrapper">
         
         <form method="post">
             <h3>Admin Login</h3>
+            <?php
+                  if ($_SERVER['REQUEST_METHOD']=='POST'){
+                    if(isset($_POST['submit'])){
+                        $username=trim($_POST['username']);
+                        $password=trim($_POST['password']);
+                        if (!$username){
+                            echo "<p>* Please enter username</p>";
+                        }
+                        if (!$password){
+                            echo "<p>* Please enter password</p>";
+                        }
+                        if($username && $password){
+                            $read = 'select * from UserInfo WHERE username ="'.$username.'" and userpwd="'.$password.'";';
+                            if($result=$con->query($read)){
+                                if($result->num_rows>0){
+                                    $_SESSION["username"] = $username;
+                                    echo $_SESSION["username"];
+                                    header("Location: user-dashboard.php");
+                                }else{
+                                    echo "<p>* Invalid username or password.</p>";
+                                }
+                            }else{
+                                echo "error.";
+                            }
+                        }
+
+
+                    // if($username=='admin' && $password=='admin'){
+                    //     header('location:admin.html');
+                    // }
+                    // else{
+                    //     echo 'Invalid username or password';
+                    // }
+                    }
+            }
+            ?>
             <label for="username">Username:</label>
             <input type="text" name="username" id="username" />
             <label for="password">Password</label>
