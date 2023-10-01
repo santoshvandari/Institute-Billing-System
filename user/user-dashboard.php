a58f541944949edd24093dec0aaa3b22b022c125
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-01 19:13:18 +0545

    Initial

--- user/user-dashboard.php
+++ user/user-dashboard.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
-    <link rel="stylesheet" href="../assets/css/user-dashboard.css"/>
+    <link rel="stylesheet" href="../assets/css/dashboard.css"/>
 
 </head>
 
