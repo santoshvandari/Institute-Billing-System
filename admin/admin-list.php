bf64c303fdbcbc04d560fbf92ca1596575f1611a
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-01 18:45:15 +0545

    Initial

--- admin/admin-list.php
+++ admin/admin-list.php
@@ -1,49 +1,99 @@
+<?php
+    session_start();
+    if(!$_SESSION['adminname']){
+        // echo "sesson not set yet";
+        header("Location: admin-login.php");
+    }
+    include_once "../assets/database/connection.php";
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Admin</title>
+    <title>Student List</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
-    <link rel="stylesheet" href="../assets/css/user-dashboard.css"/>
+    <link rel="stylesheet" href="../assets//css/user/student-list.css">
 </head>
+
 <body>
-   <header>
-    <nav>
-        <div class="logo">Billing System</div>
-        <div class="user-info">
-            <div class="user">
-                <div class="username">Santosh Bhandari</div>
-                <div class="user-img"><img src="../img/img.jpg" alt="" srcset=""></div>
+    <header>
+        <nav>
+            <div class="logo">Billing System</div>
+            <div class="user-info">
+                <div class="user">
+                    <div class="username"><?=$_SESSION["adminname"]?></div>
+                    <div class="user-img"><img src="../img/img.jpg" alt=""></div>
+                </div>
             </div>
-        </div>
-    </nav>
-   </header>
-   <main>
-    <section class="side-option">
-       <div class="side-option-container">
-        <h4>Admin</h4>
-        <ul class="side-option-list">
-            <li><a href="admin-dashboard.php">Home</a></li>
-            <li><a href="student-bill.php">Bill</a></li>
-            <li><a href="student-list.php">Student</a></li>
-            <li><a href="add-student.php">Add Student</a></li>
-            <li><a href="user-list.php">Users</a></li>
+        </nav>
+    </header>
+    <main>
+        <section class="side-option">
+            <div class="side-option-container">
+                <h4>User Dashboard</h4>
+                <ul class="side-option-list">
+                    <li><a href="user-dashboard.php">Home</a></li>
+                    <li><a href="student-bill.php">Bill</a></li>
+                    <li><a href="student-list.php">Student</a></li>
+                    <li><a href="add-student.php">Add Student</a></li>
+                    <li><a href="user-list.php">Users</a></li>
                 <li><a href="admin-list.php">Admins</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-        </div>
-           
-    </section>
-    <section class="main-container">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos aperiam reprehenderit dignissimos repellendus omnis quod iure ad, neque optio facere inventore blanditiis autem temporibus nisi sapiente minus animi laborum velit nostrum atque magnam possimus doloribus harum tenetur? Quaerat dolorem laboriosam eveniet non. Sapiente eius, aut voluptatem ab quisquam vel dolorem eos velit est maxime ex obcaecati at ea molestiae repellendus doloribus eveniet vero esse illo dolores veniam labore! Eveniet magnam accusamus eos at ratione sint quaerat nobis officiis asperiores iure unde dolore repudiandae, voluptatem modi aliquam earum. Omnis tempore dolores sequi accusantium ducimus pariatur exercitationem, nisi velit vero eius veniam!
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </section>
 
+        <section class="student-list-container main-container">
+            <div class="student-list">
+                <h3>Total Users</h3>
+                <table border="1">
+                    <thead>
+                        <tr>
 
-
-    </section>
-   </main>
-   <footer>
-   </footer>
+                            <th>S.N.</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Username</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $read= "SELECT * FROM UserInfo ORDER BY name;";
+                            // $result=$con->query($read)
+                            if ($result=$con->query($read)) {
+                                $num=0;
+                                while ($row=$result->fetch_assoc()){
+                                    $num++;
+                                    // var_dump($row);
+                                    $disp="<tr><td>$num</td>
+                                    <td>".$row['name']."</td>
+                                    <td>".$row['email']."</td>
+                                    <td>".$row['phone']."</td>
+                                    <td>".$row['username']."</td>
+                                    <td><a href='admin-dashboard.php?phone=".$row['phone']."'>Test</a></td>"; 
+                                    echo $disp;
+                                }
+                            }else{
+                                echo "Error";
+                            }
+                        
+                        
+                        
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </main>
 </body>
+</script>
 </html>
