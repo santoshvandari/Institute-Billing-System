c993110bb60d2969fc616117c24a7175cd43757b
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 17:43:46 +0545

    Initial

--- admin/edit-admin.php
+++ admin/edit-admin.php
@@ -82,6 +82,7 @@
                             
                         };
                     }
+                }
                 //        
                  if ($_SERVER['REQUEST_METHOD']=='POST'){
                     if(isset($_POST['submit'])){
