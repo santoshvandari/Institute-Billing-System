df30f551960d4136db48d16896591fa0d556dbd4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 07:26:52 +0545

    Initial

--- admin/delete-student.php
+++ admin/delete-student.php
@@ -2,17 +2,17 @@
     include_once "../assets/database/connection.php";
     if($_SERVER["REQUEST_METHOD"]=="GET"){
         if(isset($_GET["phone"])){
-            $username=trim($_GET["phone"]);
-            $delete="DELETE FROM StudentInfo WHERE phone='$username';";
+            $phone=trim($_GET["phone"]);
+            $delete="DELETE FROM StudentInfo WHERE phone='$phone';";
             if($con->query($delete)){
                 echo "Success";
-                header("Location: student-list.php?adminsuccess=success");
+                header("Location: student-list.php?studentsuccess=success");
             }else{
                 echo "Error";
-                header("Location: student-list.php?adminfailure=failure");
+                header("Location: student-list.php?studentfailure=failure");
             }
         }else{
-            header("Location: student-list.php?adminfailure=failure");
+            header("Location: student-list.php?studentfailure=failure");
         }
     }
 ?>
