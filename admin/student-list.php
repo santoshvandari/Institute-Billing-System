b9645ff38e1078a561dfb3d51c66fcc2c434a1b2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 07:22:59 +0545

    Initial

--- admin/student-list.php
+++ admin/student-list.php
@@ -21,7 +21,26 @@
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets//css/user/student-list.css">
 </head>
+<style>
+    div.message p{
+    margin: 20px  0;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 10px;
+    }
+    .success{
+        background: lightgreen;
+    }
+    .failure{
+        background: lightcoral;
+    }
+    .billMessage p{
+        color: #fff;
+    }
 
+
+</style>
 <body>
     <header>
         <nav>
