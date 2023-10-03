4aa5b4fa00d96b08ee7ee8601093e637f783207d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-03 17:38:53 +0545

    Initial

--- admin/edit-admin.php
+++ admin/edit-admin.php
@@ -68,37 +68,34 @@
         <div class="form-wrapper">
             <?php
                 if($_SERVER['REQUEST_METHOD']=='GET'){
-                    if(isset($_GET["phone"])){
-                        $phone = trim($_GET['phone']);
-                        $select = "SELECT * FROM StudentInfo WHERE phone = '$phone';";
+                    if(isset($_GET["username"])){
+                        $username = trim($_GET['username']);
+                        $select = "SELECT * FROM AdminInfo WHERE username = '$username';";
                         if($result = $con->query($select)){
                             while($row = $result->fetch_assoc()){
+                                $username=trim($row['username']);
                                 $name=trim($row['name']);
-                                $address=trim($row['address']);
                                 $email=trim($row['email']);
-                                if($email=='NULL'){
-                                    $email="";
-                                }
-                                $gender=trim($row['gender']);
-                                $parent=trim($row['parentname']);
+                                $phone=trim($row['phone']);
+                                $password=trim($row['adminpwd']);
                             };
                             
                         };
                     }
-                }
+                //        
                  if ($_SERVER['REQUEST_METHOD']=='POST'){
                     if(isset($_POST['submit'])){
                         $name=trim($_POST['name']);
                         $phone=trim($_POST['phone']);
                         $email=trim($_POST['email']);
-                        $username=trim($_POST['username']);
+                        $username=trim($_GET['username']);
                         $password=trim($_POST['password']);
+                        $update= "UPDATE AdminInfo SET name = '$name', email = '$email', phone = '$phone', adminpwd = '$password' WHERE username = '$username';";
                         
-                        $insert = "INSERT  INTO AdminInfo values('$username','$name','$email','$phone','$password');";
-                        if($con->query($insert)){
-                            echo '<div class="message"><p class="success">User Added Successfully!!</p></div>';
+                        if($con->query($update)){
+                            echo '<div class="message"><p class="success">Admin Updated Successfully!!</p></div>';
                         }else{
-                            echo '<div class="message"><p class="failure">Failed To Add User !!</p></div>';
+                            echo '<div class="message"><p class="failure">Failed To Update  Admin !!</p></div>';
                         }
                     }
                 }
@@ -110,15 +107,15 @@
         <form class="form" method="post">
             <h3>Fill the Admin Information</h3>
             <label for="name">Full Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter a Full Name" required/>
+            <input type="text" id="name" name="name" value="<?=$name?>" required/>
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Enter a Email" required/>
+            <input type="email" name="email" id="email" value="<?=$email?>" required/>
             <label for="phone">Mobile Number</label>
-            <input type="tel" name="phone" id="phone" pattern="[0-9]{10}" placeholder="Enter a Phone Number" required/>
+            <input type="tel" name="phone" id="phone" pattern="[0-9]{10}" value="<?=$phone?>" required/>
             <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Enter a Username" required/> 
+            <input type="text" name="username" id="username" value="<?=$username?>" disabled required/> 
             <label for="adminpassword">Password</label>
-            <input type="text" name="password" id="adminpassword" placeholder="Enter a Password"/>
+            <input type="text" name="password" id="adminpassword" value="<?=$password?>"/>
             <div class="btn-wrapper">
                 <button type="reset">Clear</button>
                 <button type="submit" name="submit">Submit</button>
