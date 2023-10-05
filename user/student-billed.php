6e3720dfb22e76e01e8921f54982d41cc61e6f2b
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 19:07:48 +0545

    Initial

--- user/student-billed.php
+++ user/student-billed.php
@@ -20,30 +20,24 @@
             }else{
                 $flag=false;
             }
-            // if($result=$con->query($read)){
-            //     if($result->num_rows>0){
-            //         while($row=$result->fetch_assoc()){
-            //             $name=$row["name"];
-            //             $address = $row["address"];
-            //         }
-            //     }
+
         }}
     //  }
 
 ?>
-    <title>Student Bill</title>
-    <link rel="stylesheet" href="../assets/css/common-style.css">
-    <link rel="stylesheet" href="../assets/css/user/student-billed.css">
-    <link rel="stylesheet" href="../assets/css/message.css"/>
+<title>Student Bill</title>
+<link rel="stylesheet" href="../assets/css/common-style.css">
+<link rel="stylesheet" href="../assets/css/user/student-billed.css">
+<link rel="stylesheet" href="../assets/css/message.css" />
 </head>
 <?php
     include_once "sidebar.php";
 ?>
-    <section class="main-container main-bill-container">
-        <div class="bill-wrapper">
-            <div class="bill-container">
-                <div class="billMessage">
-                    
+<section class="main-container main-bill-container">
+    <div class="bill-wrapper">
+        <div class="bill-container">
+            <div class="billMessage">
+
                 <?php
                     if($flag){
                         echo '<div class="successful"><p>Billing Process is Successful</p></div> ';
@@ -54,9 +48,9 @@
                     }
 
                 ?>
-                </div>
-                <hr>
-                <?php
+            </div>
+            <hr>
+            <?php
                     if($flag){
                         $disp1=' <div class="company-name">
                         <p>ABC Institute</p>
@@ -101,17 +95,18 @@
                     }
 
                 ?>
-               
-            </div>
+
         </div>
-    </section>
-   </main>
-   <footer>
-   </footer>
-   <script>
-   
-        document.querySelector(".date").textContent="Date: "+new Date().toLocaleDateString();
-        document.querySelector(".time").textContent="Time: "+ new Date().toLocaleTimeString();
-   </script>
+    </div>
+</section>
+</main>
+<footer>
+</footer>
+<script>
+
+    document.querySelector(".date").textContent = "Date: " + new Date().toLocaleDateString();
+    document.querySelector(".time").textContent = "Time: " + new Date().toLocaleTimeString();
+</script>
 </body>
+
 </html>
