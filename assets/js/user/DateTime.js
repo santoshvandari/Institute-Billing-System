8d64dfb7da98fa1d9f12dcc0c97e1a5a77b07ae6
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 19:10:51 +0545

    Initial

---
+++ assets/js/user/DateTime.js
@@ -0,0 +1,2 @@
+document.querySelector(".date").textContent = "Date: " + new Date().toLocaleDateString();
+    document.querySelector(".time").textContent = "Time: " + new Date().toLocaleTimeString();
