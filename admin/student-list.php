dae67d6e5af7796556e883f880b352c0d8a863a1
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 19:45:56 +0545

    Initial

--- admin/student-list.php
+++ admin/student-list.php
@@ -1,73 +1,13 @@
 <?php
-    session_start();
-    if(!$_SESSION['adminname']){
-        // echo "sesson not set yet";
-        header("Location: user-login.php");
-    }
-    include_once "../assets/database/connection.php";
-
+    include_once "head.php";
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student List</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
-    <link rel="stylesheet" href="../assets//css/user/student-list.css">
-</head>
-<style>
-    div.message p{
-    margin: 20px  0;
-    text-align: center;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 10px;
-    }
-    .success{
-        background: lightgreen;
-    }
-    .failure{
-        background: lightcoral;
-    }
-    .billMessage p{
-        color: #fff;
-    }
-
-
-</style>
-<body>
-    <header>
-        <nav>
-            <div class="logo">Billing System</div>
-            <div class="user-info">
-                <div class="user">
-                    <div class="username">Santosh Bhandari</div>
-                    <div class="user-img"><img src="../img/img.jpg" alt="" srcset=""></div>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <main>
-        <section class="side-option">
-            <div class="side-option-container">
-                <h4>Admin</h4>
-                <ul class="side-option-list">
-                    <li><a href="admin-dashboard.php">Home</a></li>
-                    <li><a href="student-bill.php">Bill</a></li>
-                    <li><a href="student-list.php">Student</a></li>
-                    <li><a href="add-student.php">Add Student</a></li>
-                    <li><a href="user-list.php">Users</a></li>
-                <li><a href="admin-list.php">Admins</a></li>
-                    <li><a href="logout.php">Logout</a></li>
-                </ul>
-            </div>
-        </section>
+    <link rel="stylesheet" href="../assets/css/user/student-list.css">
+    <link rel="stylesheet" href="../assets/css/message.css"/>
+<?php
+    include_once "sidebar.php";
+?>
 
         <section class="student-list-container main-container">
             <div class="student-list">
