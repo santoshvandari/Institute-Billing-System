fe9d2c7965084b8bca1b13d404829c4ab1e0fcc4
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 18:38:43 +0545

    Initial

--- user/user-dashboard.php
+++ user/user-dashboard.php
@@ -1,16 +1,6 @@
 <?php
-        session_start();
-        if(!$_SESSION['username']){
-            header("Location: user-login.php");
-        }
-        include_once "../assets/database/connection.php";
-    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    include_once "head.php";
+?>
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets/css/dashboard.css"/>
