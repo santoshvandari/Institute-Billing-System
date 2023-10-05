c3897d81f0d33ecbdb9add587ca95c3c7759c7d0
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 19:24:58 +0545

    Initial

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -1,35 +1,7 @@
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets/css/dashboard.css"/>
-</head>
-<body>
-   <header>
-    <nav>
-        <div class="logo">Billing System</div>
-        <div class="user-info">
-            <div class="user">
-                <div class="username">Santosh Bhandari</div>
-                <div class="user-img"><img src="../img/img.jpg" alt="" srcset=""></div>
-            </div>
-        </div>
-    </nav>
-   </header>
-   <main>
-    <section class="side-option">
-       <div class="side-option-container">
-        <h4>Admin</h4>
-        <ul class="side-option-list">
-            <li><a href="admin-dashboard.php">Home</a></li>
-            <li><a href="student-bill.php">Bill</a></li>
-            <li><a href="student-list.php">Student</a></li>
-            <li><a href="add-student.php">Add Student</a></li>
-            <li><a href="user-list.php">Users</a></li>
-                <li><a href="admin-list.php">Admins</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-        </div>
-           
-    </section>
+
         <?php
             $studentread="SELECT * FROM StudentInfo;";
             $userread="SELECT * FROM UserInfo;";
