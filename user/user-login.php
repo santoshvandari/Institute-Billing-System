f8a5bfcf15f3eec3f6bebb79a77232d55b8ab729
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 18:43:08 +0545

    Initial

--- user/user-login.php
+++ user/user-login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -52,9 +53,6 @@
                                 }
                             }
                     }
-
-
-
 ?>
             <label for="username">Username:</label>
             <input type="text" name="username" id="username" />
