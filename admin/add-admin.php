bd78a0dd7402f025828f85939671dc301346de44
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-05 19:53:22 +0545

    Initial

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -29,10 +29,6 @@
                         }
                     }
                 }
-
-
-
-
             ?>
         <form class="form" method="post">
             <h3>Fill the Admin Information</h3>
