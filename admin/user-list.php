18e7c3fcbb9b27e7cc8ee95540ba96afd6e0550a
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 09:53:38 +0545

    Initial

--- admin/user-list.php
+++ admin/user-list.php
@@ -5,38 +5,8 @@
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets//css/student-list.css">
     <link rel="stylesheet" href="../assets/css/message.css">
+    <link rel="stylesheet" href="../assets/css/admin/ButtonDesign.css">
     <script defer src="../assets/js/HideMessage.js"></script>
-
-<style>
-.btn-wrapper{
-    margin: 20px 0;
-    text-align:right;
-}
-.btn-wrapper button{
-    margin-right:20px;
-    background: #0d6efd;
-    color:#fff;
-    font-size: 20px;
-    font-weight: 400;
-    padding: 8px 20px;
-    border: 1px solid #000;
-    border-radius: 10px;
-    cursor: pointer;
-    transition: all .5s linear;
-}
-.btn-wrapper button:hover{
-    background:#41464b;
-}
-.btn-wrapper button:active{
-    background:transparent;
-    color:#000;
-}
-button a{
-    text-decoration:none;
-    color:#fff;
-}
-
-</style>
 <?php
     include_once "sidebar.php";
 ?>
