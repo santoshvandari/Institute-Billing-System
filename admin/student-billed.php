7a7a6799bfb24a9a705df4ceb3e2e3a207a75eda
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 09:12:21 +0545

    Initial

--- admin/student-billed.php
+++ admin/student-billed.php
@@ -23,15 +23,10 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets/css/user/student-billed.css">
+    <link rel="stylesheet" href="../assets/css/message.css">
 
 </head>
 <body>
