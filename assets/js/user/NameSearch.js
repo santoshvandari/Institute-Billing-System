d36e9865fa913aa2ccab2338c7d58fb93fe8ac4f
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 16:17:06 +0545

    Initial

--- assets/js/user/NameSearch.js
+++ assets/js/user/NameSearch.js
@@ -4,19 +4,19 @@ SearchInput.addEventListener("input",()=>{
     let searchvalue=SearchInput.value.trim();
     console.log(searchvalue);
     console.log("HEllo")
-    // fetch(`../../../user/read.php?name=${searchvalue}`)
-    // .then(response=>response.text())
-    // .then(response=>{
-    //     console.log(response);
-    // }).catch(e=>{
-    //     console.log(e);
-    // })
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange=()=>{
-        if(xhr.readyState==4 && xhr.status==200){
-            console.log(xhr.responseText);
-        }
-    }
-    xhr.open("GET",`/project/Institute-Billing-System/user/read.php?name=${searchvalue}`,true);
-    xhr.send();
+    fetch(`read.php?name=${searchvalue}`)
+    .then(response=>response.text())
+    .then(response=>{
+        console.log(response);
+    }).catch(e=>{
+        console.log(e);
+    })
+    // let xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange=()=>{
+    //     if(xhr.readyState==4 && xhr.status==200){
+    //         console.log(xhr.responseText);
+    //     }
+    // }
+    // xhr.open("GET",`Institute-Billing-System/user/read.php?name=${searchvalue}`,true);
+    // xhr.send();
 });
