63d1a0631a7a43e0cb09b7743c27a1574d5e1090
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 09:04:47 +0545

    Initial

--- admin/edit-bill.php
+++ admin/edit-bill.php
@@ -5,36 +5,10 @@
     <title>Edit Bill</title>
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets/css/user/studentbillform.css">
+<?php
+    include_once "sidebar.php";
 
-</head>
-<body>
-   <header>
-    <nav>
-        <div class="logo">Billing System</div>
-        <div class="user-info">
-            <div class="user">
-                <div class="username">Santosh Bhandari</div>
-                <div class="user-img"><img src="../img/img.jpg" alt="" srcset=""></div>
-            </div>
-        </div>
-    </nav>
-   </header>
-   <main>
-    <section class="side-option">
-       <div class="side-option-container">
-        <h4>Admin</h4>
-        <ul class="side-option-list">
-            <li><a href="admin-dashboard.php">Home</a></li>
-            <li><a href="student-bill.php">Bill</a></li>
-            <li><a href="student-list.php">Student</a></li>
-            <li><a href="add-student.php">Add Student</a></li>
-            <li><a href="user-list.php">Users</a></li>
-                <li><a href="admin-list.php">Admins</a></li>
-            <li><a href="logout.php">Logout</a></li>
-        </ul>
-        </div>
-           
-    </section>
+?>
     <section class="main-container form-container">
         <div class="form-wrapper">
             <div class="studentinfo">
