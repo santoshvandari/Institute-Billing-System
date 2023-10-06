eeabe862ebccca8ef83fd93a8688d3a5294e85f7
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 09:01:46 +0545

    Initial

--- admin/bill.php
+++ admin/bill.php
@@ -1,12 +1,6 @@
 <?php
-    include_once "../assets/database/connection.php";
+    include_once "head.php"
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/user/bill.css">
     <title>Student Bill</title>
 </head>
