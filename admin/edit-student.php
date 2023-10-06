bc0b87147a685a648e9efb989ceff293414e45a8
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-06 09:06:37 +0545

    Initial

--- admin/edit-student.php
+++ admin/edit-student.php
@@ -5,53 +5,9 @@
     <link rel="stylesheet" href="../assets/css/common-style.css">
     <link rel="stylesheet" href="../assets/css/user/add-student.css">
     <link rel="stylesheet" href="../assets/css/message.css">
-</head>
-<style>
-    div.message p{
-    margin: 20px  0;
-    text-align: center;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 10px;
-    }
-    .success{
-        background: lightgreen;
-    }
-    .failure{
-        background: lightcoral;
-    }
-    .billMessage p{
-        color: #fff;
-    }
-</style>
-<body>
-    <header>
-        <nav>
-            <div class="logo">Billing System</div>
-            <div class="user-info">
-                <div class="user">
-                    <div class="username">Santosh Bhandari</div>
-                    <div class="user-img"><img src="../img/img.jpg" alt="" srcset=""></div>
-                </div>
-            </div>
-        </nav>
-       </header>
-       <main>
-        <section class="side-option">
-           <div class="side-option-container">
-            <h4>Admin</h4>
-            <ul class="side-option-list">
-                <li><a href="admin-dashboard.php">Home</a></li>
-                <li><a href="student-bill.php">Bill</a></li>
-                <li><a href="student-list.php">Student</a></li>
-                <li><a href="add-student.php">Add Student</a></li>
-                <li><a href="user-list.php">Users</a></li>
-                <li><a href="admin-list.php">Admins</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
-            </div> 
-        </section>
-
+<?php
+    include_once "sidebar.php";
+?>
     <section class="form-container main-container">
         <div class="form-wrapper">
             <?php
