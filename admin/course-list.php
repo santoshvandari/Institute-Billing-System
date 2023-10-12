3c69845ed63b16e9011e400d3d24148b099be631
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-12 10:25:57 +0545

    Initial

--- admin/course-list.php
+++ admin/course-list.php
@@ -13,59 +13,87 @@
 ?>
         <section class="student-list-container main-container">
             <div class="student-list">
-                <h3>Total Admins</h3>
-                <!-- <?php
-                       if($_SERVER["REQUEST_METHOD"]=="GET"){
-                        if(isset($_GET["adminsuccess"])){
-                            echo '<div class="message"><p class="success">Admin Deleted Successfully!!</p></div>';
-                        }
-                        if(isset($_GET["adminfailure"])){
-                            echo '<div class="message"><p class="failure">Failed To Delete Admin !!</p></div>';
-                        }
-                    }
+                <h3>Total Course</h3>
+                <?php
+                    //    if($_SERVER["REQUEST_METHOD"]=="GET"){
+                    //     if(isset($_GET["adminsuccess"])){
+                    //         echo '<div class="message"><p class="success">Admin Deleted Successfully!!</p></div>';
+                    //     }
+                    //     if(isset($_GET["adminfailure"])){
+                    //         echo '<div class="message"><p class="failure">Failed To Delete Admin !!</p></div>';
+                    //     }
+                    // }
 
-                ?> -->
+                ?>
                 <table border="1">
                     <thead>
                         <tr>
 
                             <th>S.N.</th>
                             <th>Name</th>
-                            <th>Email</th>
-                            <th>Phone</th>
-                            <th>Username</th>
+                            <th>Price</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- <?php
+                        <tr>
+                            <td>1</td>
+                            <td>Computer Basic</td>
+                            <td>5000</td>
+                            <td>Edit | Delete</td>
+                        </tr> 
+                        <tr>
+                            <td>1</td>
+                            <td>Computer Basic</td>
+                            <td>5000</td>
+                            <td>Edit | Delete</td>
+                        </tr>
+                         <tr>
+                            <td>1</td>
+                            <td>Computer Basic</td>
+                            <td>5000</td>
+                            <td>Edit | Delete</td>
+                        </tr> 
+                        <tr>
+                            <td>1</td>
+                            <td>Computer Basic</td>
+                            <td>5000</td>
+                            <td>Edit | Delete</td>
+                        </tr> 
+                        <tr>
+                            <td>1</td>
+                            <td>Computer Basic</td>
+                            <td>5000</td>
+                            <td>Edit | Delete</td>
+                        </tr>
+                        <?php
                             
-                            $read= "SELECT * FROM AdminInfo ORDER BY name;";
+                            // $read= "SELECT * FROM AdminInfo ORDER BY name;";
                             // $result=$con->query($read)
-                            if ($result=$con->query($read)) {
-                                $num=0;
-                                while ($row=$result->fetch_assoc()){
-                                    $num++;
-                                    // var_dump($row);
-                                    $disp="<tr><td>$num</td>
-                                    <td>".$row['name']."</td>
-                                    <td>".$row['email']."</td>
-                                    <td>".$row['phone']."</td>
-                                    <td>".$row['username']."</td>
-                                    <td><a href='edit-admin.php?username=".$row['username']."'>Edit</a> | <a href='delete-admin.php?username=".$row['username']."'>Delete</a></td>"; 
-                                    echo $disp;
-                                }
-                            }else{
-                                echo "Error";
-                            }
+                            // if ($result=$con->query($read)) {
+                            //     $num=0;
+                            //     while ($row=$result->fetch_assoc()){
+                            //         $num++;
+                            //         // var_dump($row);
+                            //         $disp="<tr><td>$num</td>
+                            //         <td>".$row['name']."</td>
+                            //         <td>".$row['email']."</td>
+                            //         <td>".$row['phone']."</td>
+                            //         <td>".$row['username']."</td>
+                            //         <td><a href='edit-admin.php?username=".$row['username']."'>Edit</a> | <a href='delete-admin.php?username=".$row['username']."'>Delete</a></td>"; 
+                            //         echo $disp;
+                            //     }
+                            // }else{
+                            //     echo "Error";
+                            // }
                         
                         
                         
-                        ?> -->
+                        ?>
                     </tbody>
                 </table>
                 <div class="btn-wrapper">
-                    <button><a href="add-admin.php">Add Admin</a></button>
+                    <button><a href="#">Add Course</a></button>
                 </div>
             </div>
         </section>
