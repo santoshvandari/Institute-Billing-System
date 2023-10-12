2cbff20cc7b36aaa1162f8942ae42e546b6d5fc2
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-12 10:14:42 +0545

    Initial

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -16,10 +16,10 @@
         <h4>Admin</h4>
         <ul class="side-option-list">
             <li><a href="admin-dashboard.php">Home</a></li>
-            <li><a href="student-bill.php">Bill</a></li>
-            <li><a href="student-list.php">Student</a></li>
-            <li><a href="add-student.php">Add Student</a></li>
-            <li><a href="add-student.php">Course</a></li>
+            <li><a href="#">Bill</a></li>
+            <li><a href="#">Student</a></li>
+            <li><a href="#">Add Student</a></li>
+            <li><a href="#">Course</a></li>
             <!-- <li><a href="user-list.php">Users</a></li> -->
                 <li><a href="admin-list.php">Admins</a></li>
             <li><a href="logout.php">Logout</a></li>
