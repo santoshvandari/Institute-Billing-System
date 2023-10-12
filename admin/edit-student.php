910ad15560f96c7b92351f746dec3e46ae0d5bc9
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-12 19:32:29 +0545

    Initial

--- admin/edit-student.php
+++ admin/edit-student.php
@@ -13,48 +13,12 @@
     <section class="form-container main-container">
         <div class="form-wrapper">
             <?php
-                //  if ($_SERVER['REQUEST_METHOD']=='POST'){
-                //     if(isset($_POST['submit'])){
-                //         $name=trim($_POST['name']);
-                //         $address=trim($_POST['address']);
-                //         $phone=trim($_POST['phone']);
-                //         $email=trim($_POST['email']);
-                //         $gender=trim($_POST['gender']);
-                //         $parent=trim($_POST['parent']);
-                //         $cid=trim($_POST['course']);
-                //         if(!$email){
-                //             $email = "NULL";
-                //         }
-                //         $read="SELECT * FROM StudentInfo WHERE phone='$phone'";
-                //         if($result=$con->query($read)){
-                //             if($result->num_rows>0){
-                //                 echo '<div class="message"><p class="failure">Phone Number Already Added!!</p></div>';
-                //                 // echo "<script>alert('Phone Number Already Added')</script>";
-                //             }else{
-                //                 $insert = "INSERT  INTO StudentInfo values('$phone','$name','$address','$email','$gender','$parent','$cid');";
-                //                 if($con->query($insert)){
-                //                     echo '<div class="message"><p class="success">Student Record Added Successfully!!</p></div>';
-                //                 }else{
-                //                     echo '<div class="message"><p class="failure">Failed To Add Student Record!!</p></div>';
-                //                 }
-                //             }
-                //         }
-                        // $insert = "INSERT  INTO StudentInfo values('$phone','$name','$address','$email','$gender','$parent');";
-                        // if($con->query($insert)){
-                        //     echo '<div class="message"><p class="success">Student Record Added Successfully!!</p></div>';
-                        // }else{
-                        //     echo '<div class="message"><p class="failure">Failed To Add Student Record!!</p></div>';
-                        // }
-                //     }
-                // }
-
-                
-                if ($_SERVER['REQUEST_METHOD']=='GET'){
+                    if ($_SERVER['REQUEST_METHOD']=='GET'){
                         if(isset($_GET["phone"])){
                             $phone=$_GET["phone"];
                             $read="SELECT * FROM StudentInfo,CourseInfo WHERE StudentInfo.cid=CourseInfo.cid AND phone='$phone'";
                             if($result=$con->query($read)){
-                                while($result->fetch_assoc()){
+                                while($row=$result->fetch_assoc()){
                                     $name=$row['name'];
                                     $address=$row['address'];
                                     $email=$row['email'];
@@ -62,9 +26,8 @@
                                         $email="";
                                     }
                                     $gender=$row['gender'];
-                                    $parentname=$row['parent'];
+                                    $parentname=$row['parentname'];
                                     $course=$row['cname'];
-                                    $cid=$row['cid'];
                                 }
                             }
                         }else{
@@ -72,6 +35,31 @@
                         }
                 }
 
+                   if ($_SERVER['REQUEST_METHOD']=='POST'){
+                    if(isset($_POST['submit'])){
+                        $name=trim($_POST['name']);
+                        $address=trim($_POST['address']);
+                        $email=trim($_POST['email']);
+                        $phone=trim($_GET['phone']);
+                        $gender=trim($_POST['gender']);
+                        $parent=trim($_POST['parent']);
+                        if(!$email){
+                            $email = "NULL";
+                        }
+                        $update = "UPDATE  StudentInfo SET name = '$name', address = '$address', email = '$email', gender = '$gender', parentname = '$parent' WHERE phone = '$phone';";
+                        // echo $update;
+                        if($con->query($update)){
+                            echo '<div class="message"><p class="success">Student Record Updated Successfully!!</p></div>';
+                            header("refresh:5; url=student-list.php");
+                        }else{
+                            echo '<div class="message"><p class="failure">Failed To Updated Student Record!!</p></div>';
+                        }
+                    }
+                }
+
+
+                
+               
 
 
             ?>
@@ -98,9 +86,11 @@
                 ?>
             </div>
             <label for="parent">Parent Name</label>
-            <input type="text" name="parent" id="parent" value='<?=?>' required/> 
+            <input type="text" name="parent" id="parent" value='<?=$parentname?>' required/> 
             <label for="course">Course</label>
-            <select name="course" id="course" value='<?=$course?>' required></select>
+            <select name="course" id="course" disabled>
+                <option><?=$course?></option>
+            </select>
             <div class="btn-wrapper">
                 <button type="reset">Clear</button>
                 <button type="submit" name="submit">Submit</button>
