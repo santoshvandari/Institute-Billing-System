9d8483e81fe0fd41450360bfd3c7865c4798da30
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-13 18:05:00 +0545

    Initial

--- assets/database/BillingSystemold.sql
+++ assets/database/BillingSystemold.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 13, 2023 at 02:18 PM
+-- Generation Time: Oct 06, 2023 at 03:22 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `BillingSystem`
 --
+
 -- Creating Database: BillingSystem
 -- CREATE DATABASE BillingSystem;
 
@@ -28,7 +29,6 @@ SET time_zone = "+00:00";
 
 -- Granting All Permission to User on Databse BillingSystem
 -- GRANT ALL PRIVILEGES ON BillingSystem.* TO 'user'@'localhost';
-
 
 
 -- --------------------------------------------------------
@@ -50,8 +50,7 @@ CREATE TABLE `AdminInfo` (
 --
 
 INSERT INTO `AdminInfo` (`username`, `name`, `email`, `phone`, `adminpwd`) VALUES
-('admin', 'Admin', 'admin@admin.com', '9800000000', '21232f297a57a5a743894a0e4a801fc3'),
-('santoshvandari', 'Santosh Bhandari', 'santoshvandari100@gmail.com', '9824988945', '7709788ffbf21370dbeb27acff1fa383');
+('admin', 'Admin', 'admin@admin.com', '9800000000', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -61,42 +60,22 @@ INSERT INTO `AdminInfo` (`username`, `name`, `email`, `phone`, `adminpwd`) VALUE
 
 CREATE TABLE `BillInfo` (
   `phone` varchar(15) NOT NULL,
-  `cid` varchar(15) NOT NULL,
-  `amount` decimal(10,2) NOT NULL,
-  `tdate` datetime NOT NULL
+  `description` varchar(1000) NOT NULL,
+  `amount` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `BillInfo`
 --
 
-INSERT INTO `BillInfo` (`phone`, `cid`, `amount`, `tdate`) VALUES
-('9824988945', 'CA102', 5000.00, '2023-10-13 10:21:50'),
-('9824988945', 'CA102', 20000.00, '2023-10-13 10:21:54'),
-('9824988945', 'CA102', 210.00, '2023-10-13 10:21:57'),
-('9824988946', 'CA101', 1000.00, '2023-10-13 13:04:37'),
-('9824988946', 'CA101', 5000.00, '2023-10-13 17:41:44');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `CourseInfo`
---
-
-CREATE TABLE `CourseInfo` (
-  `cid` varchar(15) NOT NULL,
-  `cname` varchar(200) NOT NULL,
-  `price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `CourseInfo`
---
-
-INSERT INTO `CourseInfo` (`cid`, `cname`, `price`) VALUES
-('CA101', 'Computer Advance', 10000.00),
-('CA102', 'PHP Programming Basic', 30000.00),
-('CA103', 'JAVA Programming', 10000.00);
+INSERT INTO `BillInfo` (`phone`, `description`, `amount`) VALUES
+('9824988945', '|computer basic|computer networking|BCA', 500000.00),
+('9800000000', '|Computer Basic|Computer Programming', 10000.00),
+('9800000000', '|Compute Advace', 5000.00),
+('9800000000', '|Compute Advace', 5000.00),
+('9814003142', '|Computer Basic|Computer Hardware|Java Programming', 15000.00),
+('9814003142', '|PHP Programming', 5000.00),
+('9806079122', '|Php Programming|Java|Python|C|Author', 10000.00);
 
 -- --------------------------------------------------------
 
@@ -108,20 +87,21 @@ CREATE TABLE `StudentInfo` (
   `phone` varchar(15) NOT NULL,
   `name` varchar(50) NOT NULL,
   `address` varchar(50) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
   `gender` varchar(10) NOT NULL,
-  `parentname` varchar(50) NOT NULL,
-  `cid` varchar(15) NOT NULL
+  `parentname` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `StudentInfo`
 --
 
-INSERT INTO `StudentInfo` (`phone`, `name`, `address`, `email`, `gender`, `parentname`, `cid`) VALUES
-('9815079693', 'Manoj Oli', 'Kanakai-03', 'manojoli@gmail.com', 'male', 'Mohan Oli', 'CA103'),
-('9824988945', 'Santosh Bhandari', 'kanakai-07', 'santoshvandari100@gmail.com', 'male', 'Nanda Kumar Bhandari', 'CA102'),
-('9824988946', 'Kiran Dahal', 'kanakai-03', 'kiran@gmail.com', 'male', 'Hari Bhadur Dahal', 'CA101');
+INSERT INTO `StudentInfo` (`phone`, `name`, `address`, `email`, `gender`, `parentname`) VALUES
+('9800000000', 'Nayan Pathak', 'Birtamode-06', 'NULL', 'male', 'Radhika Pathak'),
+('9806073122', 'Ayusha Dahal', 'Birtamode', 'ayusha@gmail.com', 'female', 'Ayush Dahal'),
+('9806079122', 'Tikaram Parajuli', 'Dhulabari-10', 'tikaram@gmail.com', 'male', 'tiakram thapa parajuli'),
+('9814003142', 'Manoj Oli', 'Sarnamati', 'manoj@gmail.com', 'male', 'KP Oli'),
+('9824988945', 'santosh bhandari', 'kanakai-07', 'santoshvandari1@gmail.com', 'male', 'Nanda Kumar Bhandari');
 
 -- --------------------------------------------------------
 
@@ -143,7 +123,6 @@ CREATE TABLE `UserInfo` (
 
 INSERT INTO `UserInfo` (`username`, `name`, `email`, `phone`, `userpwd`) VALUES
 ('santoshvandari', 'Santosh Bhandari', 'santosh@santosh.com', '9824980000', '7709788ffbf21370dbeb27acff1fa383'),
-('temp', 'temp', 'temp@gmail.com', '9800000000', '3d801aa532c1cec3ee82d87a99fdf63f'),
 ('user', 'User', 'user@user.com', '9800000000', 'ee11cbb19052e40b07aac0ca060c23ee');
 
 --
@@ -160,21 +139,13 @@ ALTER TABLE `AdminInfo`
 -- Indexes for table `BillInfo`
 --
 ALTER TABLE `BillInfo`
-  ADD KEY `phone` (`phone`),
-  ADD KEY `cid` (`cid`);
-
---
--- Indexes for table `CourseInfo`
---
-ALTER TABLE `CourseInfo`
-  ADD PRIMARY KEY (`cid`);
+  ADD KEY `phone` (`phone`);
 
 --
 -- Indexes for table `StudentInfo`
 --
 ALTER TABLE `StudentInfo`
-  ADD PRIMARY KEY (`phone`),
-  ADD KEY `cid` (`cid`);
+  ADD PRIMARY KEY (`phone`);
 
 --
 -- Indexes for table `UserInfo`
@@ -190,14 +161,7 @@ ALTER TABLE `UserInfo`
 -- Constraints for table `BillInfo`
 --
 ALTER TABLE `BillInfo`
-  ADD CONSTRAINT `BillInfo_ibfk_1` FOREIGN KEY (`phone`) REFERENCES `StudentInfo` (`phone`),
-  ADD CONSTRAINT `BillInfo_ibfk_2` FOREIGN KEY (`cid`) REFERENCES `CourseInfo` (`cid`);
-
---
--- Constraints for table `StudentInfo`
---
-ALTER TABLE `StudentInfo`
-  ADD CONSTRAINT `StudentInfo_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `CourseInfo` (`cid`);
+  ADD CONSTRAINT `BillInfo_ibfk_1` FOREIGN KEY (`phone`) REFERENCES `StudentInfo` (`phone`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
