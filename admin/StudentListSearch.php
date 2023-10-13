981688ee4072a9491d126292f863f82ad656f12b
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-13 19:48:20 +0545

    Initial

--- admin/StudentListSearch.php
+++ admin/StudentListSearch.php
@@ -1,21 +1,6 @@
 <?php
     include_once "../assets/database/connection.php";
     if($_SERVER["REQUEST_METHOD"]=="GET"){
-
-
-        // $disp="<tr><td>$num</td>
-        // <td>".$row['name']."</td>
-        // <td>".$row['address']."</td>
-        // <td>".$row['gender']."</td>
-        // <td>".$row['email']."</td>
-        // <td>".$row['phone']."</td>
-        // <td>".$row['parentname']."</td>
-        // <td>".$row['cname']."</td>
-        // <td><a href='student-bill.php?phone=".$row['phone']."'>Bill</a> | <a href='edit-student.php?phone=".$row['phone']."'>Edit</a> | <a href='delete-student.php?phone=".$row['phone']."' onclick='return Check()'>Delete</a></td>"; 
-        // echo $disp;
-
-
-
         if(isset($_GET['name'])){
             $name=$_GET['name'];
             $read="SELECT * FROM StudentInfo,CourseInfo WHERE StudentInfo.cid=CourseInfo.cid AND name like '$name%';";
@@ -39,7 +24,6 @@
                     echo "<tr><td colspan='8'>No record found</td></tr>";
                 }
             }
-            // echo $read;
         }
     }
 
