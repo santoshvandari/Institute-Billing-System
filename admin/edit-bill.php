2d565953d5680969b7d582b6f0e4f3f584f8285a
Santosh Bhandari <santoshvandari1@gmail.com>
2023-10-13 17:04:30 +0545

    Initial

---
+++ admin/edit-bill.php
@@ -0,0 +1,107 @@
+<?php
+    include_once "head.php";
+?>
+    <title>Student Bill Form</title>
+    <link rel="stylesheet" href="../assets/css/common-style.css">
+    <link rel="stylesheet" href="../assets/css/studentbillform.css">
+<?php
+    include_once "sidebar.php";
+?>
+    <section class="main-container form-container">
+        <div class="form-wrapper">
+            <div class="studentinfo">
+                <h3>Student Information</h3>
+                <?php
+                if($_SERVER['REQUEST_METHOD']=='GET'){
+                    if(isset($_GET["phone"]) && isset($_GET['date'])){
+                        $phone=trim($_GET["phone"]);
+                        $tdate=trim($_GET['date']);
+                        $read="SELECT * FROM StudentInfo,CourseInfo,BillInfo WHERE CourseInfo.cid=StudentInfo.cid AND BillInfo.phone=StudentInfo.phone AND StudentInfo.phone ='$phone' AND tdate='$tdate';";
+                        if($result=$con->query($read)){
+                            if($result->num_rows>0){
+                                while($row=$result->fetch_assoc()){
+                                    $name=$row["name"];
+                                    $address=$row["address"];
+                                    $phone=$row['phone'];
+                                    $course=$row['cname'];
+                                    $amount=$row['amount'];
+                                    $courseprice=$row['price'];
+                                }
+                            }
+                        }
+                    }
+                }
+                // if($_SERVER['REQUEST_METHOD']=='POST' || $_SERVER['REQUEST_METHOD']=='GET'){
+                //    if(isset($_GET["phone"])){
+                //        $phone=trim($_GET["phone"]);
+                //     $read="SELECT * FROM StudentInfo,CourseInfo WHERE StudentInfo.cid=CourseInfo.cid AND StudentInfo.phone ='$phone';";
+                //     if($result=$con->query($read)){
+                //         $amount=0;
+                //         if($result->num_rows>0){
+                //             while($row=$result->fetch_assoc()){
+                //                 $price=$row["price"];
+                //                 $course=$row["cname"];
+                              
+                //             }
+                //             $read="SELECT * FROM BillInfo WHERE phone='$phone';";
+                //             if($result=$con->query($read)){
+                //                 if($result->num_rows>0){
+                //                     while($row=$result->fetch_assoc()){
+                //                         $amount+=(float)$row["amount"];
+                //                     }
+                //                 }
+                //             }
+                //         }
+                //     }
+                //     }else{
+                //         header("Location: student-list.php");
+                //     }
+                // }
+                ?>
+                <p class='name'>Name: <?=$name?></p>
+                <p class='address'>Address: <?=$address?></p>
+                <p class='phone'>Phone: <?=$phone?></p>
+                <p class='phone'>Course: <?=$course?></p>
+            </div>
+            <form class="form" method="post">
+                <h3>Fill the Bill Information</h3>
+                <input type="phone" name="phone" hidden value="<?=$phone?>">
+                <label for="course">Course</label>
+                <input type="text" id="course" name="course" required value='<?=$course?>' disabled/>
+                <label for="totalfee">Total Fee</label>
+                <input type="number" id="totalfee" name="totalfee" value='<?=$price?>' required disabled/>
+                <label for="dueamount">Due Amount</label>
+                <input type="number" id="dueamount" name="dueamount" value='<?=($price-$amount)?>' required disabled/>
+                <div class="errormessage"></div>
+                <label for="amount">Amount To Pay</label>
+                <input type="number" id="amount" name="amount" placeholder="Enter a Amount" required/>
+                <div class="btn-wrapper">
+                    <button type="reset">Clear</button>
+                    <button type="submit">Generate</button>
+                </div>
+            </form>
+            </div>
+    </section>
+   </main>
+</body>
+<script>
+    let totalfee = document.getElementById("totalfee").value;
+    let dueamount = document.getElementById("dueamount").value;
+    let errorMessage= document.querySelector(".errormessage");
+    let submitBtn= document.querySelector("button[type='submit']");
+    document.getElementById("amount").addEventListener("input",(e)=>{
+        let amount = Number(e.target.value);
+        console.log(amount)
+        if(amount>dueamount){
+            errorMessage.innerHTML="<p>* Amount cannot be greater than Due Amount</p>";
+            submitBtn.disabled=true;
+        }else if(amount<0 || amount==0){
+            errorMessage.innerHTML="<p>* Amount cannot be less than or Equal to 0</p>";
+            submitBtn.disabled=true;
+        }else{
+            errorMessage.innerHTML="";
+            submitBtn.disabled=false;
+        }
+    });
+</script>
+</html>
