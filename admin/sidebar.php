7667532e95c5ca11da38f0506601456d329cab82
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-05 19:05:05 +0545

    Updated

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -5,7 +5,7 @@
         <div class="logo">Billing System</div>
         <div class="user-info">
             <div class="user">
-                <div class="username"><strong><?=$_SESSION['adminname']?></strong></div>
+                <div class="username"><strong><?=$_SESSION['adminfullname']?></strong></div>
             </div>
         </div>
     </nav>
